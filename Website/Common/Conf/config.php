accdcf653d10368e44ca464cbead0d140b2c7412
yanyanshen <31381814+yanyanshen@users.noreply.github.com>
2018-01-18 15:41:52 +0800

    web site

--- Website/Common/Conf/config.php
+++ Website/Common/Conf/config.php
@@ -7,12 +7,12 @@ return array(
 
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               => '127.0.0.1', // 服务器地址
-    'DB_NAME'               => 'xueches', // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'MyNewPass4!',          // 密码
+    'DB_NAME'               => '?', // 数据库名
+    'DB_USER'               =>  '?',      // 用户名
+    'DB_PWD'                =>  '?',          // 密码
 //    'DB_PWD'                =>  'root',          // 密码
     'DB_PORT'               => 3306, // 端口
-    'DB_PREFIX'             =>  'xueches_',    // 数据库表前缀
+    'DB_PREFIX'             =>  '?',    // 数据库表前缀
     'DB_CHARSET'            => 'utf8', // 字符集*/
 //    'DB_DSN'                =>  'sqlserver:host=SqlServer;port=3306;dbname=xueche3;charset=utf8',
     'HTTP'                  =>  'http://'.$_SERVER['HTTP_HOST'],
@@ -26,7 +26,7 @@ return array(
     'APP_SUB_DOMAIN_DEPLOY'   =>    1, // 开启子域名或者IP配置
     'APP_SUB_DOMAIN_RULES'    =>    array(
         /* 域名部署配置*/
-        'www.517xc.cn'=> 'Mobile',
+        'www.?'=> 'Mobile',
         /*格式2: '子域名或泛域名或IP'=> array('模块名[/控制器名]','var1=a&var2=b&var3=*');*/
     ),
 //设置可访问的模块和默认的模块
