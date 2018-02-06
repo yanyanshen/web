7a245e1ab75313c74942cf489fb09df220fd1f20
yanyanshen <31381814+yanyanshen@users.noreply.github.com>
2018-02-06 16:45:12 +0800

    2018-02-06

--- Website/Mobile/Conf/config.php
+++ Website/Mobile/Conf/config.php
@@ -8,7 +8,7 @@ return array(
 //新版的手机网站支付宝配置参数
     'way_config' => array (
         //应用ID,您的APPID。
-        'app_id' => "2017112800228343",
+        'app_id' => "?",
 
         //商户私钥，您的原始格式RSA私钥
         'merchant_private_key' => "?"
@@ -30,14 +30,5 @@ return array(
         //支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
         'alipay_public_key' => "?"
     ),
-// 开启路由
-    'URL_ROUTER_ON'   => true,
-    'URL_ROUTER_ON '=>true,
-    'URL_ROUTE_RULES'=>array(
-        "/^[^A-Z]+(jiaxiao\/list)$/"        => 'List/pull',//列表页
-        ":name\w/jiaxiao/list/:pinyin"        => 'Detail/index',//详情页
-        ':name\w/jiaxiao/list/:r$'          =>  'List/pull/r/1',//推荐筛选
-        ':name\w/jiaxiao/list/:all$'          =>  'List/pull/all/1',//推荐
-        ":name\w/jiaxiao/list/:pinyin\w#detail"        => 'Detail/index',//详情页
-    ),
+
 );
